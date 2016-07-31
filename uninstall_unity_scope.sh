4dfc1b95d55c2ad5c979cf63fa3c5a1aa6da4ebe
dusty-nv <dustinf@nvidia.com>
2016-07-31 18:24:19 +0000

    added script to uninstall / disable Unity search lenses/scopes

---
+++ uninstall_unity_scope.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# note:  to remove online search (wikipedia, amazon, ect.)
+#        go to 'Security & Privacy' settings -> Search tab
+#        and disable Online search
+
+sudo apt-get remove --purge unity-lens-friends
+sudo apt-get remove --purge unity-lens-music
+sudo apt-get remove --purge unity-lens-music
+sudo apt-get remove --purge unity-lens-photos
+sudo apt-get remove --purge unity-lens-video
+
+sudo apt-get remove --purge unity-scope-audacious
+sudo apt-get remove --purge unity-scope-calculator
+sudo apt-get remove --purge unity-scope-chromiumbookmarks
+sudo apt-get remove --purge unity-scope-clementine
+sudo apt-get remove --purge unity-scope-colourlovers
+sudo apt-get remove --purge unity-scope-devhelp
+sudo apt-get remove --purge unity-scope-firefoxbookmarks
+sudo apt-get remove --purge unity-scope-gdrive
+sudo apt-get remove --purge unity-scope-gmusicbrowser
+sudo apt-get remove --purge unity-scope-gourmet
+sudo apt-get remove --purge unity-scope-guayadeque
+sudo apt-get remove --purge unity-scope-manpages
+sudo apt-get remove --purge unity-scope-musicstores
+sudo apt-get remove --purge unity-scope-musique
+sudo apt-get remove --purge unity-scope-openclipart
+sudo apt-get remove --purge unity-scope-exdoc
+sudo apt-get remove --purge unity-scope-tomboy
+sudo apt-get remove --purge unity-scope-video-remote
+sudo apt-get remove --purge unity-scope-virtualbox
+sudo apt-get remove --purge unity-scope-yelp
+sudo apt-get remove --purge unity-scope-zotero
+
